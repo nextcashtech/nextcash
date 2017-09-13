18493385c6f7e63bf4edb2eb97dd08c0a6fd01d6
Curtis Ellis <curtis@arcmist.com>
2017-09-13 17:00:27 -0600

    Add parameter that can be passed to threads when starting them

--- src/base/thread.cpp
+++ src/base/thread.cpp
@@ -10,44 +10,75 @@
 
 namespace ArcMist
 {
-    std::thread::id *Thread::mMainThreadID = NULL;
-    std::map<std::thread::id, char *> Thread::mThreadNames;
+    std::thread::id *Thread::sMainThreadID = NULL;
+    std::map<std::thread::id, String> Thread::sThreadNames;
+    std::map<std::thread::id, void *> Thread::sThreadParameters;
 
-    Thread::Thread(const char *pName, void (*pFunction)()) : mThread(pFunction)
+    Thread::Thread(const char *pName, void (*pFunction)(), void *pParameter) : mThread(pFunction)
     {
-        if(mMainThreadID == NULL)
-            mMainThreadID = new std::thread::id(std::this_thread::get_id());
+        if(sMainThreadID == NULL)
+            sMainThreadID = new std::thread::id(std::this_thread::get_id());
 
-        mName = new char[std::strlen(pName)+1];
-        std::strcpy(mName, pName);
-        mThreadNames[mThread.get_id()] = mName;
-        Log::addFormatted(Log::VERBOSE, THREAD_LOG_NAME, "Starting thread : %s", mName);
+        mName = pName;
+        sThreadNames[mThread.get_id()] = pName;
+        sThreadParameters[mThread.get_id()] = pParameter;
+        Log::addFormatted(Log::VERBOSE, THREAD_LOG_NAME, "Starting thread : %s", mName.text());
     }
 
     Thread::~Thread()
     {
-        Log::addFormatted(Log::VERBOSE, THREAD_LOG_NAME, "Stopping thread : %s", mName);
+        Log::addFormatted(Log::VERBOSE, THREAD_LOG_NAME, "Stopping thread : %s", mName.text());
         mThread.join();
-        std::map<std::thread::id, char *>::iterator pair = mThreadNames.find(mThread.get_id());
-        if(pair != mThreadNames.end())
-            mThreadNames.erase(pair);
-        delete[] mName;
+        std::map<std::thread::id, String>::iterator name = sThreadNames.find(mThread.get_id());
+        if(name != sThreadNames.end())
+            sThreadNames.erase(name);
+        std::map<std::thread::id, void *>::iterator parameter = sThreadParameters.find(mThread.get_id());
+        if(parameter != sThreadParameters.end())
+            sThreadParameters.erase(parameter);
     }
 
-    const char *Thread::currentName()
+    const char *Thread::currentName(int pTimeoutMilliseconds)
     {
-        if(mMainThreadID == NULL)
-            mMainThreadID = new std::thread::id(std::this_thread::get_id());
+        if(sMainThreadID == NULL)
+            sMainThreadID = new std::thread::id(std::this_thread::get_id());
 
         std::thread::id currentID = std::this_thread::get_id();
-        if(currentID == *mMainThreadID)
+        if(currentID == *sMainThreadID)
             return "Main";
 
-        std::map<std::thread::id, char *>::iterator pair = mThreadNames.find(currentID);
-        if(pair != mThreadNames.end())
-            return pair->second;
-
+        std::map<std::thread::id, String>::iterator name = sThreadNames.find(currentID);
+        // Wait for it to be set
+        while(name == sThreadNames.end() && pTimeoutMilliseconds > 0)
+        {
+            name = sThreadNames.find(currentID);
+            sleep(100);
+            pTimeoutMilliseconds -= 100;
+        }
+        if(name != sThreadNames.end())
+            return name->second.text();
         return "Unknown";
+    }
+
+    void *Thread::currentParameter(int pTimeoutMilliseconds)
+    {
+        if(sMainThreadID == NULL)
+            sMainThreadID = new std::thread::id(std::this_thread::get_id());
+
+        std::thread::id currentID = std::this_thread::get_id();
+        if(currentID == *sMainThreadID)
+            return NULL;
+
+        std::map<std::thread::id, void *>::iterator parameter = sThreadParameters.find(currentID);
+        // Wait for it to be set
+        while(parameter == sThreadParameters.end() && pTimeoutMilliseconds > 0)
+        {
+            parameter = sThreadParameters.find(currentID);
+            sleep(100);
+            pTimeoutMilliseconds -= 100;
+        }
+        if(parameter != sThreadParameters.end())
+            return parameter->second;
+        return NULL;
     }
 
     void Thread::sleep(unsigned int pMilliseconds)
--- src/base/thread.hpp
+++ src/base/thread.hpp
@@ -1,6 +1,8 @@
 #ifndef ARCMIST_THREAD_HPP
 #define ARCMIST_THREAD_HPP
 
+#include "string.hpp"
+
 #include <thread>
 #include <map>
 
@@ -11,22 +13,26 @@ namespace ArcMist
     {
     public:
 
-        // Current thread name
-        static const char *currentName();
+        // Name specified when the thread that is currently executing was created
+        static const char *currentName(int pTimeoutMilliseconds = 1000);
+        // Parameter specified when the thread that is currently executing was created. NULL if not specified
+        static void *currentParameter(int pTimeoutMilliseconds = 1000);
+        // Have this thread sleep for specified milliseconds
         static void sleep(unsigned int pMilliseconds);
 
-        Thread(const char *pName, void (*pFunction)());
+        Thread(const char *pName, void (*pFunction)(), void *pParameter = NULL);
         ~Thread();
 
         const char *name() { return mName; }
 
     private:
 
-        char *mName;
+        String mName;
         std::thread mThread;
 
-        static std::thread::id *mMainThreadID;
-        static std::map<std::thread::id, char *> mThreadNames;
+        static std::thread::id *sMainThreadID;
+        static std::map<std::thread::id, String> sThreadNames;
+        static std::map<std::thread::id, void *> sThreadParameters;
 
     };
 }
