1851822448d18fba3a0a0d0c5d3b57af53f91ee4
Curtis Ellis <curtis@arcmist.com>
2018-02-21 15:40:45 -0700

    Fix readers lock to actually provide exclusive write locks.

--- src/base/mutex.cpp
+++ src/base/mutex.cpp
@@ -77,13 +77,38 @@ namespace ArcMist
 
     void ReadersLock::writeLock(const char *pRequestName)
     {
-        // Notify that writer lock is needed
-        mMutex.lock();
-        mWriterWaiting = true;
-        mMutex.unlock();
+        int sleeps = 0;
+
+        // Wait for the writers to unlock
+        while(true)
+        {
+            mMutex.lock();
+            if(!mWriterWaiting && !mWriterLocked)
+            {
+                mWriterWaiting = true;
+                mMutex.unlock();
+                break;
+            }
+
+            if(++sleeps > 500)
+            {
+                // It has been over 5 seconds. So notify that this wait is taking too long
+                if(mWriterLocked)
+                    Log::addFormatted(Log::VERBOSE, "Mutex", "Waiting for write lock for %s on %s (write locked by %s)",
+                      pRequestName, mName.text(), mWriteLockName);
+                else
+                    Log::addFormatted(Log::VERBOSE, "Mutex", "Waiting for write lock for %s on %s (other writer waiting)",
+                      pRequestName, mName.text());
+                sleeps = 0;
+            }
+
+            // Wait for readers to unlock
+            mMutex.unlock();
+            usleep(10000); // 1/100th of a second
+        }
 
         // Wait for the readers to unlock
-        int sleeps = 0;
+        sleeps = 0;
         while(true)
         {
             mMutex.lock();
@@ -98,7 +123,7 @@ namespace ArcMist
 
             if(++sleeps > 500)
             {
-                // It has been over a second. So notify that this wait is taking too long
+                // It has been over 5 seconds. So notify that this wait is taking too long
                 if(pRequestName != NULL)
                     Log::addFormatted(Log::VERBOSE, "Mutex", "Waiting for write lock for %s on %s (%d readers locked)",
                       pRequestName, mName.text(), mReaderCount);
